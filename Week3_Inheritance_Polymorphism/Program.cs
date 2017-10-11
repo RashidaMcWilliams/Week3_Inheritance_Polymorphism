11d89c1ce2b960ec71d56c57135ef16e3b39403d
McShi <rashida.mcwilliams@gmail.com>
2017-10-11 14:21:44 -0400

    Added code for new automobile object

--- Week3_Inheritance_Polymorphism/Program.cs
+++ Week3_Inheritance_Polymorphism/Program.cs
@@ -10,13 +10,20 @@ namespace Week3_Inheritance_Polymorphism
     {
         static void Main(string[] args)
         {
-            Vehicle baseVehicle = new Vehicle(4, 2000, "Green", 55.0d);
+            //Vehicle baseVehicle = new Vehicle(4, 2000, "Green", 55.0d);
 
-            baseVehicle.Move();
-            Console.WriteLine(baseVehicle.DistancedTraveled);
+            //baseVehicle.Move();
+            //baseVehicle.Move();
+            //Console.WriteLine(baseVehicle.DistancedTraveled);
+
+            Automobile herbie = new Automobile(4, 13, 2, 4, 1500, "White", 88.0d);
+            herbie.Move();
+            herbie.Move();
+            Console.WriteLine("Automobile Distance Traveled: " + herbie.DistancedTraveled);
+
+
+
+
         }
-
-
-
     }
 }
